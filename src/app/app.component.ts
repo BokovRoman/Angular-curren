29eb6b8afc9d559a6eae50afd5c7617a2dd9903e
BokovRoman <r.bokov1991@gmail.com>
2022-06-26 02:14:42 +0300

    refactoring code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CurrencycalculatorService } from './currencycalculator.service';
 
 @Component({
@@ -6,60 +6,35 @@ import { CurrencycalculatorService } from './currencycalculator.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'currency';
-  curjson: any = [];
-  displayVal:string=''
-  getValue(val:string) {
-    console.warn(val)
-    this.displayVal=val
-  }
-
-
-  base = "USD";
-  count2 = "USD";
-  result: string = '1';
-  currencyRate: string = ''
-
-
-  changebase(a: string) {
-    this.base = a;
-    // console.log(this.base);
-  }
-
-    tocountry(b: string) {
-    this.count2 = b;
-    // console.log(this.count2);
-    }
+export class AppComponent{
+  currencyRateDollar: any = this.currency.getcurrencyDollar();
+  currencyRateEuro: any = this.currency.getcurrencyEuro();
+  inputValue: any = '';
+  currencies: any = 'USD EUR UAH'.split(' ');
+  selectedCurrency: any = 'USD';
+  inputResult: any = '';
+  selectedCurrencyResult: any = 'USD';
   
   constructor(private currency:CurrencycalculatorService) {}
-  
-  convert() {
-    // console.log(this.base);
-    // console.log(this.count2);
-    this.currency.getcurrencydata(this.base).subscribe(data => {
-      // console.log(data);
-      this.curjson = JSON.stringify(data);
-      // console.log(this.curjson);
-      this.curjson = JSON.parse(this.curjson);
-      console.log(this.curjson);
-
-      this.currencyRate=this.curjson.rates.UAH
-        console.log(this.displayVal)
-      if (this.count2 === "USD") {
-        this.result = this.curjson.rates.USD
-        
-      }
-
-      if (this.count2 === "EUR") {
-        this.result=this.curjson.rates.EUR
-        }
-         
-      if (this.count2 === "UAH") {
-        this.result = this.curjson.rates.UAH
-      }
-  
+   
+  inputValueChange(value: any) {
+    this.inputValue = value;
+    this.currency.getcurrency(this.selectedCurrency).subscribe((data: any)=> {
+      this.inputResult = value * data.rates[this.selectedCurrencyResult];
     })
+  }
 
+  currencyBaseChange(value: any) {
+    this.selectedCurrency = value;
+    this.currency.getcurrency(this.selectedCurrency).subscribe((data: any)=> {
+      this.inputResult = this.inputValue * data.rates[this.selectedCurrencyResult];
+    })
+  }
+
+  currencyResultChange(value: any) {
+    this.selectedCurrencyResult = value;
+    this.currency.getcurrency(this.selectedCurrency).subscribe((data: any)=> {
+      this.inputResult = this.inputValue * data.rates[this.selectedCurrencyResult];
+    })
   }
 }
--- src/app/currencycalculator.service.ts
+++ src/app/currencycalculator.service.ts
@@ -1,14 +1,34 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencycalculatorService {
 
+  currencyUrl: string = 'https://api.exchangerate.host/latest?base=';
+
   constructor(private http: HttpClient) { }
-  getcurrencydata(country1: string) {
-    let url = "https://api.exchangerate.host/latest?base=" + country1
-    return this.http.get(url);
+
+  getcurrencyDollar() {
+    return this.http.get(`${this.currencyUrl}USD`).pipe(
+      map((data: any) => {
+        return data.rates.UAH;
+      })
+    );
   }
+
+  getcurrencyEuro() {
+        return this.http.get(`${this.currencyUrl}EUR`).pipe(
+      map((data: any) => {
+        return data.rates.UAH;
+      })
+    );
+  }
+  
+    getcurrency(base:string) {
+        return this.http.get(`${this.currencyUrl}${base}`)
+   }
+  
 }
